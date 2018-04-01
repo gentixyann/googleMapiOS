ec8d2665b259c30d77c2b47925b1dac162309396
Gento <a551.return@gmail.com>
2018-04-01 22:55:53 +0900

    change for original app

--- googleMapiOS/RestaurantPreviewView.swift
+++ googleMapiOS/RestaurantPreviewView.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import WebKit
 
 class RestaurantPreviewView: UIView {
     
@@ -20,9 +21,9 @@ class RestaurantPreviewView: UIView {
     }
     
     func setData(title: String, img: UIImage, price: Int) {
-        lblTitle.text = title
-        imgView.image = img
-        lblPrice.text = "$\(price)"
+        //lblTitle.text = title
+        //imgView.image = img
+        //lblPrice.text = "$\(price)"
     }
     
     func setupViews() {
@@ -32,23 +33,23 @@ class RestaurantPreviewView: UIView {
         containerView.rightAnchor.constraint(equalTo: rightAnchor).isActive=true
         containerView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive=true
         
-        containerView.addSubview(lblTitle)
-        lblTitle.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 0).isActive=true
-        lblTitle.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 0).isActive=true
-        lblTitle.rightAnchor.constraint(equalTo: containerView.rightAnchor, constant: 0).isActive=true
-        lblTitle.heightAnchor.constraint(equalToConstant: 35).isActive=true
-        
-        addSubview(imgView)
-        imgView.leftAnchor.constraint(equalTo: leftAnchor).isActive=true
-        imgView.topAnchor.constraint(equalTo: lblTitle.bottomAnchor).isActive=true
-        imgView.rightAnchor.constraint(equalTo: rightAnchor).isActive=true
-        imgView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive=true
-        
-        addSubview(lblPrice)
-        lblPrice.centerXAnchor.constraint(equalTo: centerXAnchor).isActive=true
-        lblPrice.centerYAnchor.constraint(equalTo: imgView.centerYAnchor).isActive=true
-        lblPrice.widthAnchor.constraint(equalToConstant: 90).isActive=true
-        lblPrice.heightAnchor.constraint(equalToConstant: 40).isActive=true
+//        containerView.addSubview(lblTitle)
+//        lblTitle.leftAnchor.constraint(equalTo: containerView.leftAnchor, constant: 0).isActive=true
+//        lblTitle.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 0).isActive=true
+//        lblTitle.rightAnchor.constraint(equalTo: containerView.rightAnchor, constant: 0).isActive=true
+//        lblTitle.heightAnchor.constraint(equalToConstant: 35).isActive=true
+//
+//        addSubview(imgView)
+//        imgView.leftAnchor.constraint(equalTo: leftAnchor).isActive=true
+//        imgView.topAnchor.constraint(equalTo: lblTitle.bottomAnchor).isActive=true
+//        imgView.rightAnchor.constraint(equalTo: rightAnchor).isActive=true
+//        imgView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive=true
+//
+//        addSubview(lblPrice)
+//        lblPrice.centerXAnchor.constraint(equalTo: centerXAnchor).isActive=true
+//        lblPrice.centerYAnchor.constraint(equalTo: imgView.centerYAnchor).isActive=true
+//        lblPrice.widthAnchor.constraint(equalToConstant: 90).isActive=true
+//        lblPrice.heightAnchor.constraint(equalToConstant: 40).isActive=true
     }
     
     let containerView: UIView = {
@@ -57,36 +58,47 @@ class RestaurantPreviewView: UIView {
         return v
     }()
     
-    let imgView: UIImageView = {
-        let v=UIImageView()
-        v.image=#imageLiteral(resourceName: "restaurant1")
+//    let imgView: UIImageView = {
+//        let v=UIImageView()
+//        v.image=#imageLiteral(resourceName: "restaurant1")
+//        v.translatesAutoresizingMaskIntoConstraints=false
+//        return v
+//    }()
+    
+    
+    let imgView: WKWebView = {
+        let v=WKWebView()
         v.translatesAutoresizingMaskIntoConstraints=false
         return v
     }()
     
-    let lblTitle: UILabel = {
-        let lbl=UILabel()
-        lbl.text = "Name"
-        lbl.font=UIFont.boldSystemFont(ofSize: 28)
-        lbl.textColor = UIColor.black
-        lbl.backgroundColor = UIColor.white
-        lbl.textAlignment = .center
-        lbl.translatesAutoresizingMaskIntoConstraints=false
-        return lbl
-    }()
     
-    let lblPrice: UILabel = {
-        let lbl=UILabel()
-        lbl.text="$12"
-        lbl.font=UIFont.boldSystemFont(ofSize: 32)
-        lbl.textColor=UIColor.white
-        lbl.backgroundColor=UIColor(white: 0.2, alpha: 0.8)
-        lbl.textAlignment = .center
-        lbl.layer.cornerRadius = 5
-        lbl.clipsToBounds=true
-        lbl.translatesAutoresizingMaskIntoConstraints=false
-        return lbl
-    }()
+    
+    
+    
+//    let lblTitle: UILabel = {
+//        let lbl=UILabel()
+//        lbl.text = "Name"
+//        lbl.font=UIFont.boldSystemFont(ofSize: 28)
+//        lbl.textColor = UIColor.black
+//        lbl.backgroundColor = UIColor.white
+//        lbl.textAlignment = .center
+//        lbl.translatesAutoresizingMaskIntoConstraints=false
+//        return lbl
+//    }()
+//
+//    let lblPrice: UILabel = {
+//        let lbl=UILabel()
+//        lbl.text="$12"
+//        lbl.font=UIFont.boldSystemFont(ofSize: 32)
+//        lbl.textColor=UIColor.white
+//        lbl.backgroundColor=UIColor(white: 0.2, alpha: 0.8)
+//        lbl.textAlignment = .center
+//        lbl.layer.cornerRadius = 5
+//        lbl.clipsToBounds=true
+//        lbl.translatesAutoresizingMaskIntoConstraints=false
+//        return lbl
+//    }()
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
