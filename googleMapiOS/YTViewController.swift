323a78c91655550e6d0d0a3df8977ae8495bc57e
Gento <a551.return@gmail.com>
2018-04-01 16:20:19 +0900

    change to WebKit View. Because WebView was deprecated

--- googleMapiOS/YTViewController.swift
+++ googleMapiOS/YTViewController.swift
@@ -7,20 +7,28 @@
 //
 
 import UIKit
+import WebKit
 
 class YTViewController: UIViewController {
 
+    
+    @IBOutlet var myWebView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
+        getVideo(videoCode: "OVGbAFy36xM")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func getVideo(videoCode: String){
+        let url = URL(string: "https://www.youtube.com/embed/\(videoCode)")
+        myWebView.load(URLRequest(url: url!))
     }
     
 
